e7b319b0f9571dcbac691f7a00315ea404a882a4
chocomelo24 <mansoorchanique@gmail.com>
2022-08-04 11:18:50 +0200

    update

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,88 +1,147 @@
 const express = require("express");
 const router = express.Router();
 const con = require("../lib/db_connection");
-const jwt = require("jsonwebtoken");
 const middleware = require("../middleware/auth");
-const nodemailer =
-
-// get all users
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 router.get("/", (req, res) => {
-    try {
-        con.query("SELECT * FROM users", (err, result) => {
-            if (err) throw err;
-            res.send(result);
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error)
-    }
-  
+  try {
+    con.query("SELECT * FROM users", (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 });
-
-// get one user
-router.get("/:id", (req, res) => {
-    try {
-        con.query(`SELECT * FROM users where user_id = ${req.params.id}`, (err, result) => {
-            if (err) throw err;
-            res.send(result);
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error)
-    }
-});
-
-// add
 router.post("/", (req, res) => {
-    const {email, password, full_name, billing_address, default_shipping_address, country, phone, user_type} = req.body;
-    try {
-        con.query(
-            //When using the ${}, the content of con.query MUST be in the back tick
-            `INSERT INTO users (email, password, full_name, billing_address, default_shipping_address, country, phone, user_type) VALUES ("${email}", "${password}", "${full_name}", "${billing_address}", "${default_shipping_address}", "${country}", "${phone}", "${user_type}")`,
-            (err, result) => {
-              if (err) throw err;
-              res.send(result);
-            }
-          );
-        } catch (error) {
-          console.log(error);
-          res.status(400).send(error);
-    } 
+  const {
+    email,
+    password,
+    full_name,
+    billing_address,
+    default_shipping_address,
+    country,
+    phone,
+    user_type,
+  } = req.body;
+  try {
+    con.query(
+      `insert into users (email,password,full_name,billing_address,default_shipping_address,country,phone,user_type) values ('${email}', '${password}', '${full_name}', '${billing_address}', '${default_shipping_address}', '${country}', '${phone}', '${user_type}') `,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 });
-
-const bcrypt = require('bcryptjs');
-
+//gets one user
+router.get("/:id", (req, res) => {
+  try {
+    con.query(
+      `SELECT * FROM users where user_id = ${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+//delete
+router.delete("/:id", (req, res) => {
+  try {
+    con.query(
+      `delete from users where user_id = ${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+//edit
+router.put("/:id", (req, res) => {
+  const {
+    email,
+    password,
+    full_name,
+    billing_address,
+    default_shipping_address,
+    country,
+    phone,
+    user_type,
+  } = req.body;
+  try {
+    con.query(
+      `update users set email = "${email}",  password = "${password}", full_name = "${full_name}", billing_address = "${billing_address}", default_shipping_address = "${default_shipping_address}", country = "${country}", phone = "${phone}", user_type = "${user_type}"  where user_id = "${req.params.id}"`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+router.patch("/", (req, res) => {
+  const { email, password } = req.body;
+  try {
+    con.query(
+      `select * from users where email = "${email}" and  password = "${password}"`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+const bcrypt = require("bcryptjs");
 // Register Route
 // The Route where Encryption starts
 router.post("/register", (req, res) => {
   try {
     let sql = "INSERT INTO users SET ?";
+    //This is the body im requesting
     const {
       full_name,
       email,
       password,
       user_type,
       phone,
-      country, 
+      country,
       billing_address,
       default_shipping_address,
     } = req.body;
-
     // The start of hashing / encryption
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
-
+    //Database terms
     let user = {
       full_name,
       email,
       // We sending the hash value to be stored witin the table
-      password:hash,
+      password: hash,
       user_type,
       phone,
       country,
       billing_address,
       default_shipping_address,
     };
+    //SQL Query
+    //connection to database
     con.query(sql, user, (err, result) => {
       if (err) throw err;
       console.log(result);
@@ -92,55 +151,8 @@ router.post("/register", (req, res) => {
     console.log(error);
   }
 });
-
-
 // Login
 // The Route where Decryption happens
-// router.post("/login", (req, res) => {
-//   try {
-//     let sql = "SELECT * FROM users WHERE ?";
-//     let user = {
-//       email: req.body.email,
-//     };
-//     con.query(sql, user, async (err, result) => {
-//       if (err) throw err;
-//       if (result.length === 0) {
-//         res.send("Email not found please register");
-//       } else {
-//         // Decryption
-//         // Accepts the password stored in database and the password given by user (req.body)
-//         // const isMatch = await bcrypt.compare(
-//           //   req.body.password,
-//           //   result[0].password
-//           // );
-//           const isMatch = () =>{
-//             if (req.body.password===result[0].password){
-//               true
-//             }
-//             else{
-//               false
-//             }
-            
-//           }
-
-
-
-//         // If password does not match
-//         if (!isMatch) {
-//           res.send("Password incorrect");
-//         }
-//         else {
-//           res.send(result)
-//         }
-//       }
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-
-// Login
 router.post("/login", (req, res) => {
   try {
     let sql = "SELECT * FROM users WHERE ?";
@@ -190,32 +202,128 @@ router.post("/login", (req, res) => {
   } catch (error) {
     console.log(error);
   }
-})
+});
 // Verify
 router.get("/users/verify", (req, res) => {
-    const token = req.header("x-auth-token");
-    jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
-      if (error) {
-        res.status(401).json({
-          msg: "Unauthorized Access!",
-        });
-      } else {
-        res.status(200);
-        res.send(decodedToken);
-      }
-    });
-  });
- 
-  router.get("/", middleware, (req, res) => {
-    try {
-      let sql = "SELECT * FROM users";
-      con.query(sql, (err, result) => {
-        if (err) throw err;
-        res.send(result);
+  const token = req.header("x-auth-token");
+  jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
+    if (error) {
+      res.status(401).json({
+        msg: "Unauthorized Access!",
       });
-    } catch (error) {
-      console.log(error);
+    } else {
+      res.status(200);
+      res.send(decodedToken);
     }
   });
-
+});
+router.get("/", middleware, (req, res) => {
+  try {
+    let sql = "SELECT * FROM users";
+    con.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.post("/forgot-psw", (req, res) => {
+  try {
+    let sql = "SELECT * FROM users WHERE ?";
+    let user = {
+      email: req.body.email,
+    };
+    con.query(sql, user, (err, result) => {
+      if (err) throw err;
+      if (result === 0) {
+        res.status(400), res.send("Email not found");
+      } else {
+        // Allows me to connect to the given email account || Your Email
+        const transporter = nodemailer.createTransport({
+          host: process.env.MAILERHOST,
+          port: process.env.MAILERPORT,
+          auth: {
+            user: process.env.MAILERUSER,
+            pass: process.env.MAILERPASS,
+          },
+        });
+        // How the email should be sent out
+        var mailData = {
+          from: process.env.MAILERUSER,
+          // Sending to the person who requested
+          to: result[0].email,
+          subject: "Password Reset",
+          html: `<div>
+            <h3>Hi ${result[0].full_name},</h3>
+            <br>
+            <h4>Click link below to reset your password</h4>
+            <a href="https://user-images.githubusercontent.com/4998145/52377595-605e4400-2a33-11e9-80f1-c9f61b163c6a.png">
+              Click Here to Reset Password
+              user_id = ${result[0].user_id}
+            </a>
+            <br>
+            <p>For any queries feel free to contact us...</p>
+            <div>
+              Email: ${process.env.MAILERUSER}
+              <br>
+              Tel: If needed you can add this
+            <div>
+          </div>`,
+        };
+        // Check if email can be sent
+        // Check password and email given in .env file
+        transporter.verify((error, success) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email valid! ", success);
+          }
+        });
+        transporter.sendMail(mailData, (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            res.send("Please Check your email", result[0].user_id);
+          }
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+// Rest Password Route
+router.put("reset-psw/:id", (req, res) => {
+  let sql = "SELECT * FROM users WHERE ?";
+  let user = {
+    user_id: req.params.id,
+  };
+  con.query(sql, user, (err, result) => {
+    if (err) throw err;
+    if (result === 0) {
+      res.status(400), res.send("User not found");
+    } else {
+      let newPassword = `UPDATE users SET ? WHERE user_id = ${req.params.id}`;
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+      const updatedPassword = {
+        full_name: result[0].full_name,
+        email: result[0].email,
+        user_type: result[0].user_type,
+        phone: result[0].phone,
+        country: result[0].country,
+        billing_address: result[0].billing_address,
+        default_shipping_address: result[0].default_shipping_address,
+        // Only thing im changing in table
+        password: hash,
+      };
+      con.query(newPassword, updatedPassword, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Password Updated please login");
+      });
+    }
+  });
+});
 module.exports = router;
